f3d0757b09d8e3b5d785375f2004a5314a0d06a5
tien anh <giakhang22102016@gmail.com>
2019-10-02 13:15:39 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FilterExample';
+   
+  list:Company[] =[{
+    "name" :"FHS",
+    "address":"Taiwan",
+    "phone" :"09238723786"
+  },
+  {
+    "name" :"Asian Tech",
+    "address":"Vietname",
+    "phone" :"984859845949"
+  },{
+    "name" :"Smartviet",
+    "address":"USA",
+    "phone" :"9588985650"
+  },{
+    "name" :"FPT",
+    "address":"hongkong",
+    "phone" :"94859459484"
+  },{
+    "name" :"Angular",
+    "address":"japan",
+    "phone" :"509059659650695"
+  }]
+
+}
+export class Company{
+   name: string;
+   address: string;
+   phone: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,Ng2SearchPipeModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
